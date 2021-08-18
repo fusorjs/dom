9eec00295f9747db37bce0954077ee3699d8cd5a
Igor Sukharev <imixaly4@gmail.com>
2021-08-18 07:31:51 +0200

    refactor & optimize init children

--- src/child/init.ts
+++ src/child/init.ts
@@ -2,73 +2,88 @@ import {Child, some, isFunction, isLiteral} from '@perform/common';
 
 import {Renderer, ChildUpdater} from '../types';
 
-// includes undefined, true coz ||
+// includes undefined, true because of ||
 const isSkipable = (v: unknown) => v === false || v == null || v === '' || v === true;
 
-// todo refactor
+type RenderedChild = Child<Renderer> | ReturnType<Renderer>;
+
+type Setter = (parentNode: Node, child: RenderedChild, prevNode: Node) => Node;
+
+const initComponent: Setter = (parentNode, child) => {
+  parentNode.appendChild(child as Node);
+  return child as Node;
+};
+
+const updateComponent: Setter = (parentNode, child, prevNode) => {
+  parentNode.replaceChild(child as Node, prevNode);
+  return child as Node;
+};
+
+const initLiteral: Setter = (parentNode, child) => {
+  const text = document.createTextNode(child as string);
+  parentNode.appendChild(text);
+  return text;
+}
+
+const updateLiteral: Setter = (parentNode, child, prevNode) => {
+  const text = document.createTextNode(child as string);
+  parentNode.replaceChild(text, prevNode);
+  return text;
+}
+
+const update = (
+  callback: () => Child<Renderer>, parentNode: Node,
+  prevChild: RenderedChild, prevNode: Node,
+  component: Setter, literal: Setter, recursed = 0,
+): [prevChild: RenderedChild, prevNode: Node] => {
+  let child: RenderedChild = callback();
+
+  if (child instanceof HTMLElement) { // component renderer
+    if (prevChild !== child) prevNode = component(parentNode, child, prevNode);
+  }
+  else if (isSkipable(child)) { // before: literal as '', function as null
+    child = '';
+    if (prevChild !== child) prevNode = literal(parentNode, child, prevNode);
+  }
+  else if (isLiteral(child)) {
+    if (prevChild !== child) prevNode = literal(parentNode, child, prevNode);
+  }
+  else if (isFunction(child as some)) { // condition
+    if (recursed === 5) throw new Error(`prevent indefinite loop: ${recursed++}`);
+    return update(
+      callback, parentNode,
+      prevChild, prevNode,
+      component, literal, recursed++
+    );
+  }
+  else throw new Error(`unsupported child: ${callback}`);
+
+  prevChild = child;
+
+  return [prevChild, prevNode]
+};
 
 const createUpdater = (
-  callback: () => Child<Renderer>, parent: HTMLElement
+  callback: () => Child<Renderer>, parentNode: Node,
 ) => {
+  let prevChild: RenderedChild;
   let prevNode: Node;
-  let prevChild: Child<Renderer>;
 
-  const update = (recursed = 0) => {
-    let child = callback();
+  // init
+  [prevChild, prevNode] = update(
+    callback, parentNode,
+    prevChild, prevNode!, // we do not use prevNode on init...
+    initComponent, initLiteral
+  );
 
-    if (child instanceof HTMLElement) { // component renderer
-      if (prevNode) { // update
-        if (prevChild !== child) {
-          parent.replaceChild(child, prevNode);
-          prevNode = child;
-        }
-      }
-      else { // init
-        parent.append(child);
-        prevNode = child;
-      }
-    }
-    else if (isSkipable(child)) { // before: literal as '', function as null
-      child = '';
-      if (prevNode) { // update
-        if (prevChild !== child) {
-          const text = document.createTextNode(child);
-          parent.replaceChild(text, prevNode);
-          prevNode = text;
-        }
-      }
-      else { // init
-        const text = document.createTextNode(child);
-        parent.append(text);
-        prevNode = text;
-      }
-    }
-    else if (isLiteral(child)) {
-      if (prevNode) { // update
-        if (prevChild !== child) {
-          const text = document.createTextNode(child as string);
-          parent.replaceChild(text, prevNode);
-          prevNode = text;
-        }
-      }
-      else { // init
-        const text = document.createTextNode(child as string);
-        parent.append(text);
-        prevNode = text;
-      }
-    }
-    else if (isFunction(child as some)) { // condition
-      if (recursed === 5) throw new Error(`prevent indefinite loop: ${recursed++}`);
-      update(recursed++);
-    }
-    else throw new Error(`unsupported child: ${callback}`);
-
-    prevChild = child;
+  return () => {
+    // update
+    [prevChild, prevNode] = update(
+      callback, parentNode,
+      prevChild, prevNode,
+      updateComponent, updateLiteral
+    );
   };
-
-  update(); // init
-
-  return update;
 };
 
 export const initChildren = (
